03fc56c93d0ae4aed7097b7f5aed7d253aff0238
nickgarlis <nickgarlis@gmail.com>
2018-11-03 23:42:57 +0200

    Add int_to_size_t function & use size_t types

--- read_write.c
+++ read_write.c
@@ -1,49 +1,61 @@
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
+#include <unistd.h>
 #include <string.h>
 
-int get_file (char *file_name);
-void read_file (int file, int buff_size);
-void close_file (int file);
+int open_file (const char *path, const int permissions);
+void read_file (const int file_des, const size_t buff_size);
+void close_file (const int file_des);
+size_t int_to_size_t(const int val);
 
 int main (int argc, char *argv[]) {
-    const short int start = atoi(argv[1]);
-    const short int buff_size = atoi(argv[2]);
+    const unsigned int start = atoi(argv[1]);
+    const size_t buff_size = int_to_size_t(atoi(argv[2]));
 
-    const int file = get_file("data2.txt");
-    
-    read_file(file, buff_size);
+    const int data_file = open_file("data2.txt", O_RDONLY);
 
-    close_file(file);
+    read_file(data_file, buff_size);
+
+    close_file(data_file);
 
     return 0;
 }
 
-int get_file (char *file_name) {
-    int const status = open(file_name, O_RDONLY);
-    if (status<0) {
+int open_file (const char *path, const int permissions) {
+    int const file_des = open(path, permissions);
+    if (file_des < 0) {
         perror("Cannot open file");
         exit(1);
     }
-    return status;
+    return file_des;
 }
 
-void read_file (int file, int buff_size) {
+void read_file (const int file_des, const size_t buff_size) {
     char buffer[buff_size];
-	
     memset(buffer, 0, buff_size);
-    while (read(file, buffer, buff_size-1)){
+    ssize_t read_chars = read(file_des, buffer, buff_size-1);
+    if (read_chars<0) {
+        perror("Cannot read file");
+        exit(1);
+    }
+    while (read_chars) {
         printf("%s\n", buffer);
         memset(buffer, 0, buff_size);
+        read_chars = read(file_des, buffer, buff_size-1);
 	}
 }
 
-void close_file (int file) {
+void close_file (const int file) {
     int const status = close(file);
     if (status<0) {
         perror("Cannot close file");
         exit(1);
     }
+}
+
+size_t int_to_size_t (const int val) {
+    return (val < 0) ? __SIZE_MAX__ : (size_t)((unsigned)val);
 }
